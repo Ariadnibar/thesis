294a612e261f0bc46c88891d0d1f03c3361ef5b5
Ariadnibar <ariadnibar@gmail.com>
2024-02-23 00:26:53 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='flex h-screen flex-col items-center justify-center'>
+      <h1 className='text-3xl font-semibold'>thesis-frontend</h1>
+    </div>
+  );
+};
+
+export default App;
